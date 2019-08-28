59a06bf7a475487047717c7c3d93fbc66d8e95ae
richardcrng@gmail.com <richardcrng@gmail.com>
2019-08-28 16:43:06 +0100

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let index = 0
+  window.addEventListener('keydown', function(event) {
+    if (parseInt(event.which) === code[index]) {
+      index++
+      if (index === code.length) {
+        window.alert('win!')
+      }
+    } else {
+      window.alert('NO!')
+      index = 0
+    }
+  })
 }
